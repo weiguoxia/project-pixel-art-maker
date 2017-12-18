75352464d432d154757995506e7eafbc34f35765
weiguo xia <xiaw@miamioh.edu>
2017-12-18 13:32:54 -0500

    implement pixel maker

--- designs.js
+++ designs.js
@@ -1,10 +1,21 @@
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
-
-function makeGrid() {
-
-// Your code goes here!
-
+function returnTable(height, width, color = '') {
+  var r ='';
+  for(let j = 0; j < height; j++) {
+    for (let i = 0; i < width; i++ ) {
+      if(i ===0)r += '<tr>';
+      r += '<td ' + 'id=' + j + '.' +i + ' style="background-color:' + color + '"> </td>';
+      if(i ==width-1)r += '</tr>';
+    }
+  }
+  return r;
 }
+
+function makeGrid(form) {
+  var height = form.input_height.value;
+  var width = form.input_width.value;
+  $('tr').remove();
+  $('table').append(returnTable(height, width));
+}
+$('#container' ).on( 'click', 'td', function(event) {
+  $(event.target).css("background-color", $("#colorPicker").val() );
+});
